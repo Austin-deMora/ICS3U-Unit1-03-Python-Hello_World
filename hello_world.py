15263e4b043eae5d53bba203067651baeed6e63c
Your Name <you@example.com>
2021-04-23 14:35:17 +0000

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Austin de Mora
+# Created on: April 2021
+# This is the "Hello, World!" Program, with proper style.
+
+
+def main():
+    print('Hello, World!')
+    print('Goodbye, World!')
+
+
+if __name__ == "__main__":
+    main()
